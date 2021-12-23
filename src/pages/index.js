50487589965dfd3be25540005013b5f0077b0e02
Ewan <ewan.lad@hotmail.com>
2021-12-23 23:29:28 +0000

    [ADD] Stars

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
-import React from "react"
+import React from "react";
+
+import "./index.scss";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="night">
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+    </div>
+  );
 }

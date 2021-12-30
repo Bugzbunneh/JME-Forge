20a97d61fce7eed43240ae9781edb65cea55a6fc
Ewan <ewan.lad@hotmail.com>
2021-12-30 13:32:06 +0000

    [ADD] Initial JME

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,22 @@
 import React from "react";
+import instagramLogo from "./images/instagram.jpeg";
 
 import "./index.scss";
 
 export default function Home() {
   return (
-    <div className="night">
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
+    <div className="main-container">
+      <div className="flex-container">
+        <div className="jme-circle">
+          <div className="jme-circle-text">
+            <p>
+              <b>J.M.E</b>
+              <div className="line"></div>
+            </p>
+            <p>FORGE</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
